8362d0bf6179d164d77d1b97cca37cab83642159
Manish Jha <manishjha5410@gmail.com>
2021-04-07 19:41:39 +0530

    Update Multistage Graph.cpp

--- Multistage Graph/Multistage Graph.cpp
+++ Multistage Graph/Multistage Graph.cpp
@@ -34,33 +34,3 @@ int main()
 
     cout<<"The shortest distance between source and distance is "<<multiStage(g);
 }
-
-
-/*
-8 12
-0 1 1
-0 2 2
-0 3 5
-1 4 4
-1 5 11
-2 4 9
-2 5 5
-2 6 16
-3 6 2
-4 7 18
-5 7 13
-6 7 2
-
-7 11
-0 1 3
-0 3 2
-0 2 8
-1 4 7
-1 3 5
-2 5 6
-3 2 4
-3 4 5
-3 5 12
-4 6 5
-5 6 2
-*/
